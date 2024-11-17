9b6e67197a044be651cfc7b28d150c3fe7c3652c
LTP <lamphat279@gmail.com>
2024-11-17 23:58:46 +0700

    update chat top

--- Services/GeminiApiService.cs
+++ Services/GeminiApiService.cs
@@ -158,8 +158,8 @@ Your response (in Vietnamese):";
                 },
                 generationConfig = new
                 {
-                    temperature = 0.7,
-                    topK = 40,
+                    temperature = 0.9,
+                    topK = 50,
                     topP = 0.95,
                     maxOutputTokens = 2048,
                 }
